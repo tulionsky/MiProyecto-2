ffc06bc87da1a95962eaad43cc7683064281dfd3
Tulio <tquintanaa@miumg.edu.gt>
2024-02-22 18:56:06 -0600

    Mostrar un Mensaje de Aprobación o Rechazo de Préstamo:

--- MiProyecto#2/codigos/Program.cs
+++ MiProyecto#2/codigos/Program.cs
@@ -1,22 +1,25 @@
-﻿int num1;
-bool error = true;
+﻿bool error = true;
 Console.WriteLine("Hola Amigo!");
-Console.WriteLine("Este es un programa para ver cual de los 3 numeros es el mayor");
+Console.WriteLine("Este es un programa para ver si su prestamo es aprobado o no.");
 while (error)
 {
     try
     {
-        Console.WriteLine("Porfavor ingrese un numero:");
-        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el monto del préstamo: ");
+        decimal montoPrestamo = Convert.ToDecimal(Console.ReadLine());
 
-        if (num1 % 2 == 0)
+        Console.Write("Ingrese su edad: ");
+        int edadUsuario = Convert.ToInt32(Console.ReadLine());
+
+        if (montoPrestamo < 5000 || edadUsuario > 60)
         {
-            Console.WriteLine("El numero ingresado es PAR");
+            Console.WriteLine("Felicidades, su préstamo ha sido aprobado.");
         }
         else
         {
-            Console.WriteLine("El numero ingresado es IMPAR");
+            Console.WriteLine("Lo sentimos, su solicitud de préstamo ha sido rechazada.");
         }
+
         error = false;
     }
     catch (Exception ex)
