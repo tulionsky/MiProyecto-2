5bc524fb8424564ff69d3bec7fb3faae8c005851
Tulio <tquintanaa@miumg.edu.gt>
2024-02-22 18:28:21 -0600

    Agregar archivos de proyecto.

---
+++ MiProyecto#2/1/Program.cs
@@ -0,0 +1,38 @@
+﻿int num1, num2, num3;
+bool error = true;
+Console.WriteLine("Hola Amigo!");
+Console.WriteLine("Este es un programa para ver cual de los 3 numeros es el mayor");
+while (error)
+{
+    try
+    {
+        Console.WriteLine("Por favor ingresa el primer numero:");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Por favor ingresa el segundo numero:");
+        num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Por favor ingresa el tercer numero:");
+        num3 = int.Parse(Console.ReadLine());
+        if (num1 > num2 && num1 > num3)
+        {
+            Console.WriteLine("El numero " + num1 + " es el mayor.");
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            Console.WriteLine("El numero " + num2 + " es el mayor.");
+        }
+        else if ((num3 > num2) && (num3 > num1))
+        {
+            Console.WriteLine("El numero " + num3 + " es el mayor.");
+        }
+        else 
+        {
+            Console.WriteLine("Todos son iguales");
+        }
+        error= false;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Uy! Parece que hubo un error, Lo quieres intentar de nuevo?");
+        error= true;
+    }
+}
